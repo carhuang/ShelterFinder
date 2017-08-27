1e895848dc06d1fafc6c12e94f3eeb91a5acfe11
c85615 <c85615@gmail.com>
2017-08-27 16:30:48 -0700

    set up index page and google map

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/**
+ * Created by carlyhuang on 2017-08-27.
+ */
+$(document).ready(function () {
+    /*
+    var map;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 2,
+            center: new google.maps.LatLng()
+        })
+    }
+    */
+}
+
+
+
+);
